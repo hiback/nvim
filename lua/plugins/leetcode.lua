42ad7c17a62383d6ef55c1cd0cfb7ea565c423ed
hiback <5335514+hiback@users.noreply.github.com>
2024-12-04 14:03:36 +0900

    Added leetcode

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,26 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    -- "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    -- configuration goes here
+    ---@type lc.lang
+    lang = "cpp",
+    ---@type string
+    arg = "leet",
+    ---@type lc.storage
+    storage = {
+      home = "~/leetcode",
+      cache = "~/leetcode/.cache",
+    },
+  },
+}
